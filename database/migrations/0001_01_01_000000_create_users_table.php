4c38e96b5c24169c801b79b6788a4530fe3eb5ca
Deepak Sharma <deepak.sharma5@dotsquares.com>
2026-02-07 18:30:53 +0530

    changes

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,8 +17,10 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('status')->default(1)->comment('1=Active,0=Inactive');
             $table->rememberToken();
             $table->timestamps();
+            $table->integer('plan_id')->nullable();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
